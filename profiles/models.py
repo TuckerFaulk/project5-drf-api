bc580a616634148fc4a354b9167330a36a8d1d2b
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-03-01 16:23:03 +0000

    Create profiles app and model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_klqque'
+    )
+    address = models.TextField(blank=True)
+    is_admin = models.BooleanField(default=False)
+
+    fire_alarm = models.BooleanField(default=False)
+    emergency_lighting = models.BooleanField(default=False)
+    sprinkler_system = models.BooleanField(default=False)
+    gas = models.BooleanField(default=False)
+    asbestos = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
