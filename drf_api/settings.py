0fbf393d8c636236ffa8b026327c9059a833d390
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-03-15 15:09:17 +0000

    Add secret_key to env.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6!7ll0jgajg$iv1pbu@5n0l=7(8i0y+)e%@4fkc4^qz&l_)1wh'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
