3f193badc0d9a63dadc4935842612961696a2144
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-03-21 18:29:07 +0000

    Removed permission_classes from UserTaskList View

--- user_tasks/views.py
+++ user_tasks/views.py
@@ -6,17 +6,19 @@ from drf_api.permissions import IsAssignedToOrReadOnly
 
 class UserTaskList(generics.ListAPIView):
     """
+    Lists all user tasks
     No POST method as User Tasks are either created when initially assigned
     or when a user task is closed and the frequency is not 'once'
+    No permissions class is required as there is no POST Method available
     """
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Need to check whether this is the correct class
     queryset = UserTask.objects.all()
     serializer_class = UserTaskSerializer
 
 
 class UserTaskDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-
+    Retrieve, update or delete a user task if it is assigned to the logged in
+    user or an admin user is logged in
     """
     permission_classes = [IsAssignedToOrReadOnly | permissions.IsAdminUser]
     queryset = UserTask.objects.all()
