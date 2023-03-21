4ae32055a81aeca82b78a6dca7d6fde806aeb7a8
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-03-21 16:19:06 +0000

    Add create_task_action signal to the actions model

--- actions/models.py
+++ actions/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from categories.models import Category
+from user_tasks.models import UserTask
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Action(models.Model):
@@ -17,7 +20,7 @@ class Action(models.Model):
         User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    due_date = models.DateField(blank=False)
+    due_date = models.DateField(null=True, blank=True)
     risk_rating = models.CharField(
         max_length=6, choices=risk_rating_filter_choices, default='low'
     )
@@ -31,3 +34,15 @@ class Action(models.Model):
 
     def __str__(self):
         return f"{self.action_title} - {self.assigned_to}"
+
+
+@receiver(post_save, sender=UserTask)
+def create_task_action(sender, instance, created, **kwargs):
+    if created is False and instance.status == "closed" and instance.action_required:
+
+        Action.objects.create(
+            action_title=instance.task_name.task_name,
+            category=instance.task_name.category,
+            description=instance.action_description,
+            assigned_to=instance.assigned_to.assigned_to,
+        )
