4ac3f40e45130ca6432d71f88c16a11d13849978
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-03-24 13:36:45 +0000

    Add filterset fields to Comment List Views

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import TaskComment, ActionComment
 from .serializers import TaskCommentSerializer, TaskCommentDetailSerializer, ActionCommentSerializer, ActionCommentDetailSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
@@ -8,9 +9,11 @@ class TaskCommentList(generics.ListCreateAPIView):
     """
     Lists all user task comments.
     """
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = TaskComment.objects.all()
     serializer_class = TaskCommentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['task_name']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -29,9 +32,11 @@ class ActionCommentList(generics.ListCreateAPIView):
     """
     Lists all user action comments.
     """
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = ActionComment.objects.all()
     serializer_class = ActionCommentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['action_title']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
