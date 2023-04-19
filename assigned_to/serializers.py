962f3984181e37246db9d1a471b541ebc1adf043
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-04-19 17:27:22 +0000

    Update masterTask and assignedTo serlializers to display instance names not IDs

--- assigned_to/serializers.py
+++ assigned_to/serializers.py
@@ -7,11 +7,20 @@ class AssignedToSerializer(serializers.ModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    task_name = serializers.SerializerMethodField()
+    assigned_to = serializers.SerializerMethodField()
 
     # Source: Code Institutes Django REST Framework Videos
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    # Source: https://stackoverflow.com/questions/63130043/how-to-get-field-name-instead-of-foreign-id-in-django
+    def get_task_name(self, instance):
+        return instance.task_name.task_name
+
+    def get_assigned_to(self, instance):
+        return instance.assigned_to.username
 
     class Meta:
         model = AssignedTo
--- master_tasks/serializers.py
+++ master_tasks/serializers.py
@@ -5,11 +5,16 @@ from .models import MasterTask
 class MasterTaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
 
     # Source: Code Institutes Django REST Framework Videos
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    # Source: https://stackoverflow.com/questions/63130043/how-to-get-field-name-instead-of-foreign-id-in-django
+    def get_category(self, instance):
+        return instance.category.category_name
 
     class Meta:
         model = MasterTask
